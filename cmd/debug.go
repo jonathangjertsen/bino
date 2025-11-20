467b18e951c6091aa1365eed92ffe77eb5ee6912
Jonathan Gjertsen <jonathan.gjertsen@disruptive-technologies.com>
2025-11-20 01:34:56 +0100

    Some more entries for machine, error handling

--- cmd/debug.go
+++ cmd/debug.go
@@ -12,6 +12,7 @@ import (
 	"github.com/shirou/gopsutil/v3/disk"
 	"github.com/shirou/gopsutil/v3/host"
 	"github.com/shirou/gopsutil/v3/load"
+	"github.com/shirou/gopsutil/v3/mem"
 )
 
 type DebugInfo struct {
@@ -34,15 +35,37 @@ func (server *Server) debugHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	data := MustLoadCommonData(ctx)
 
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
+	// Process info
+	var procMem runtime.MemStats
+	runtime.ReadMemStats(&procMem)
 
-	avg, _ := load.Avg()
-	u, _ := disk.Usage("/")
-	h, _ := host.Info()
-	users, _ := host.Users()
-	cwd, _ := os.Getwd()
+	// Machine info
+	avg, err := load.Avg()
+	if err != nil {
+		LogCtx(ctx, "getting machine Avg: %v", err)
+	}
+	u, err := disk.Usage("/")
+	if err != nil {
+		LogCtx(ctx, "getting machine Disk usage: %v", err)
+	}
+	h, err := host.Info()
+	if err != nil {
+		LogCtx(ctx, "getting machine Info: %v", err)
+	}
+	users, err := host.Users()
+	if err != nil {
+		LogCtx(ctx, "getting machine Users: %v", err)
+	}
+	cwd, err := os.Getwd()
+	if err != nil {
+		LogCtx(ctx, "getting machine Getwd: %v", err)
+	}
+	mem, err := mem.VirtualMemory()
+	if err != nil {
+		LogCtx(ctx, "getting machine VirtualMemory: %v", err)
+	}
 
+	// Build info
 	buildInfo := []DebugInfo{}
 	if info, ok := debug.ReadBuildInfo(); ok {
 		for _, setting := range info.Settings {
@@ -56,12 +79,15 @@ func (server *Server) debugHandler(w http.ResponseWriter, r *http.Request) {
 			Children: []DebugInfo{
 				{Name: "NumCPU", Value: runtime.NumCPU()},
 				{Name: "System total load avg (up to 100% * n cores)", Value: fmt.Sprintf("%.1f", avg.Load1*100)},
-				{Name: "Disk total GB", Value: toGB(u.Total)},
-				{Name: "Disk free GB", Value: toGB(u.Free)},
-				{Name: "Disk used GB", Value: toGB(u.Used)},
+				{Name: "Disk total (GB)", Value: toGB(u.Total)},
+				{Name: "Disk available (GB)", Value: toGB(u.Free)},
+				{Name: "Disk used (GB)", Value: toGB(u.Used)},
 				{Name: "Hostname", Value: h.Hostname},
 				{Name: "Uptime", Value: data.User.Language.FormatTimeAbsWithRelParen(time.Unix(int64(h.BootTime), 0))},
 				{Name: "Public IP", Value: server.Runtime.PublicIP},
+				{Name: "Memory total (MB)", Value: toMB(mem.Total)},
+				{Name: "Memory available (MB)", Value: toMB(mem.Available)},
+				{Name: "Memory used (MB)", Value: toMB(mem.Used)},
 				{Name: "Users", Children: SliceToSlice(users, func(in host.UserStat) DebugInfo {
 					return DebugInfo{
 						Name:  "Username",
@@ -75,8 +101,8 @@ func (server *Server) debugHandler(w http.ResponseWriter, r *http.Request) {
 			Children: []DebugInfo{
 				{Name: "Goroutines", Value: runtime.NumGoroutine()},
 				{Name: "Started", Value: data.User.Language.FormatTimeAbsWithRelParen(server.Runtime.TimeStarted)},
-				{Name: "Alloc MB", Value: toMB(m.Alloc)},
-				{Name: "Total MB", Value: toMB(m.Sys)},
+				{Name: "Alloc MB", Value: toMB(procMem.Alloc)},
+				{Name: "Total MB", Value: toMB(procMem.Sys)},
 				{Name: "Working directory", Value: cwd},
 			},
 		},
