bd3685c9c1fabdfafeb9971f2b12b6a0aac09ef4
Jonathan Gjertsen <jonathan.gjertsen@disruptive-technologies.com>
2025-11-20 01:27:28 +0100

    Reorganize debug info, add users

--- cmd/debug.go
+++ cmd/debug.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"runtime"
 	"runtime/debug"
+	"time"
 
 	"github.com/shirou/gopsutil/v3/disk"
 	"github.com/shirou/gopsutil/v3/host"
@@ -39,6 +40,7 @@ func (server *Server) debugHandler(w http.ResponseWriter, r *http.Request) {
 	avg, _ := load.Avg()
 	u, _ := disk.Usage("/")
 	h, _ := host.Info()
+	users, _ := host.Users()
 	cwd, _ := os.Getwd()
 
 	buildInfo := []DebugInfo{}
@@ -50,31 +52,32 @@ func (server *Server) debugHandler(w http.ResponseWriter, r *http.Request) {
 
 	info := []DebugInfo{
 		{
-			Name: "Process",
-			Children: []DebugInfo{
-				{Name: "Goroutines", Value: runtime.NumGoroutine()},
-				{Name: "NumCPU", Value: runtime.NumCPU()},
-				{Name: "Started", Value: data.User.Language.FormatTimeAbsWithRelParen(server.Runtime.TimeStarted)},
-				{Name: "Alloc MB", Value: toMB(m.Alloc)},
-				{Name: "Total MB", Value: toMB(m.Sys)},
-				{Name: "Working directory", Value: cwd},
-			},
-		},
-		{
 			Name: "Machine",
 			Children: []DebugInfo{
+				{Name: "NumCPU", Value: runtime.NumCPU()},
 				{Name: "System total load avg (up to 100% * n cores)", Value: fmt.Sprintf("%.1f", avg.Load1*100)},
 				{Name: "Disk total GB", Value: toGB(u.Total)},
 				{Name: "Disk free GB", Value: toGB(u.Free)},
 				{Name: "Disk used GB", Value: toGB(u.Used)},
 				{Name: "Hostname", Value: h.Hostname},
-				{Name: "Uptime", Value: h.Uptime},
+				{Name: "Uptime", Value: data.User.Language.FormatTimeAbsWithRelParen(time.Unix(int64(h.BootTime), 0))},
+				{Name: "Public IP", Value: server.Runtime.PublicIP},
+				{Name: "Users", Children: SliceToSlice(users, func(in host.UserStat) DebugInfo {
+					return DebugInfo{
+						Name:  "Username",
+						Value: in.User,
+					}
+				})},
 			},
 		},
 		{
-			Name: "Network",
+			Name: "Process",
 			Children: []DebugInfo{
-				{Name: "Public IP", Value: server.Runtime.PublicIP},
+				{Name: "Goroutines", Value: runtime.NumGoroutine()},
+				{Name: "Started", Value: data.User.Language.FormatTimeAbsWithRelParen(server.Runtime.TimeStarted)},
+				{Name: "Alloc MB", Value: toMB(m.Alloc)},
+				{Name: "Total MB", Value: toMB(m.Sys)},
+				{Name: "Working directory", Value: cwd},
 			},
 		},
 		{
